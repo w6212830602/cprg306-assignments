f1faa454f51270c17f5172abaa7ec8b5d18d31d5
w6212830602 <w6212830602@gmail.com>
2025-05-13 12:00:58 -0600

    Update week 2 assignment

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="./week-2">Week 2</Link>
       
     </main>
