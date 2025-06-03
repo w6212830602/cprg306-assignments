5d1291ee91be09e602e3fc4f01a4a048547e0135
w6212830602 <w6212830602@gmail.com>
2025-06-03 11:58:47 -0600

    week-5

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2">Week 2</Link>
-      <Link href="./week-3">Week 3</Link>
-      <Link href="./week-4">Week 4</Link>
+      <Link href="./week-2">Week 2 </Link>
+      <Link href="./week-3">Week 3 </Link>
+      <Link href="./week-4">Week 4 </Link>
+      <Link href="./week-5">Week 5 </Link>
       
     </main>
   );
