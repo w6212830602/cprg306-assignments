01a7a1b6554e217e89c3ffabebad0b7bd9b0b601
w6212830602 <w6212830602@gmail.com>
2025-06-10 11:57:20 -0600

    week-6

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Link href="./week-3">Week 3 </Link>
       <Link href="./week-4">Week 4 </Link>
       <Link href="./week-5">Week 5 </Link>
+      <Link href="./week-6">Week 6 </Link>
       
     </main>
   );
